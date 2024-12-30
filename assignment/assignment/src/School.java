4b32b90bcd82fe881b5899a962edf4f355a23508
disasq <saqypdias@gmail.com>
2024-12-30 15:49:29 +0500

    Add files via upload

---
+++ assignment/assignment/src/School.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class School {
+    private List<Person> members;
+
+    public School() {
+        this.members = new ArrayList<>();
+    }
+
+    public void addMember(Person person) {
+        members.add(person);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Person member : members) {
+            sb.append(member.toString()).append("\n");
+        }
+        return sb.toString();
+    }
+
+    public void sortMembersBySurname() {
+        members.sort(Comparator.comparing(Person::getSurname));
+    }
+}
